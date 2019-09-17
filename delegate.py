b527a4b9d355df5600a9a50e8820902f200bb93c
jayantjain100 <jayantjain100@gmail.com>
2019-09-17 13:22:11 +0530

    verification for no

--- delegate.py
+++ delegate.py
@@ -3,15 +3,17 @@ from nfa import NFA
 from verifier import invoke_verifier
 import numpy as np
 
-NUM_TRIALS = 1
+NUM_TRIALS = 10
 
 if __name__ == '__main__':
 	
 	# invoke_prover()
-	my_nfa = NFA(5, ['a', 'b'], {'a':{0:[1,2], 4:[3]}, 'b':{0:[3], 1:[1,4]}, 'eps':{2:[3,1], 3:[2]}}, [4])
-	input_string = 'abababbbbbbbbbbbbbbbbbbbbbbbbbbbbbbba'
-	# known_nfa = my_nfa.get_similar_nfa(input_string)
-	known_nfa = my_nfa
+	my_nfa = NFA(3, ['a', 'b'], {'a': {0:[1]}, 'b':{1:[2]}} , [2], 0)
+	my_nfa.complete()
+	input_string = 'abbb'
+	# my_nfa = NFA(5, ['a', 'b'], {'a':{0:[1,2], 4:[3]}, 'b':{0:[3], 1:[1,4]}, 'eps':{2:[3,1], 3:[2]}}, [4])
+	known_nfa = my_nfa.get_similar_nfa(input_string)
+	# known_nfa = my_nfa
 
 	indexes = [0 for i in range(NUM_TRIALS)] + [1 for i in range(NUM_TRIALS)]
 
--- nfa.py
+++ nfa.py
@@ -8,7 +8,7 @@ from encryption import decrypt
 import os
 import numpy as np
 from cryptography.fernet import Fernet
-
+import random
 
 class NFA:
 	def __init__(self,
@@ -23,17 +23,25 @@ class NFA:
 		#considering sparse
 		#an undefined transition implies rejection of string (going to a non accepting dump which loops to itslef for all input)
 
-		self.num_states = num_states
+		#have to make gods nfa so that i can actuaaly choose a random path without a problem
+		self.num_states = num_states 
 		self.alphabet = alphabet
 
 		#eg = {'a':{1:[3,4] , 3:[2,1]}, 'c':{2:[0]}, 'eps':{0:[1,2,3,4]}} this is the form of delta
 		#it is a dictionary which maps chars to the tables of transitions that happen on that specific char
 		#here each table maps prev to possible_nexts
+
+		#PE - someone specifying an empty entry in the description as in delta[c] = {} pehle se aajaye
 		true_alphabet = delta.keys()
 		for char in alphabet:
 			if (char not in true_alphabet):
 				delta[char] = {}
-
+		# for char in alphabet:
+		# 	temp = { i:[(self.num_states-1)] for i in range(self.num_states)}
+		# 	if (char in true_alphabet):
+		# 		for k in delta[char]:
+		# 			temp[k] = delta[char][k]
+		# 	delta[char] = temp
 		if('eps' not in true_alphabet):
 			delta['eps'] = {}
 
@@ -41,12 +49,31 @@ class NFA:
 		self.final_states = final_states
 		self.start_state = start_state
 
+	def complete(self):
+		self.num_states += 1
+		for char in self.alphabet:
+			temp = { i:[(self.num_states-1)] for i in range(self.num_states)}
+			for k in self.delta[char]:
+				temp[k] = self.delta[char][k]
+			self.delta[char] = temp
+
 	def draw(self):
 		#calls the graphviz module whith appropriate args
 		pass
 
 	def get_similar_nfa(self, input_string):
-		new_final_states = self.final_states
+		current = self.start_state
+		for i in range(len(input_string)):
+			char_of_interest = input_string[i]
+			possible_nexts =  self.delta[char_of_interest][current]
+			current = random.choice(possible_nexts)
+			#PE , LATER find the epsilon closure and choose something random, for true randomness
+			# print(self.delta)
+			if current in self.delta['eps'].keys():
+				possible_nexts = (self.delta['eps'][current].values()) + [current]
+				current = random.choice(possible_nexts)
+
+		new_final_states = [current]
 		return NFA(self.num_states, self.alphabet, self.delta, new_final_states, self.start_state)
 
 	def garble(self, input_string, security_param = 32):
